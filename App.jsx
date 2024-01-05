9ea7507aecdb88d0adcb043b42e92533f9edec5c
ivandayrit <100040157+ivan06292002@users.noreply.github.com>
2024-01-05 15:13:55 +0800

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Hello from './components/hello'
+import Welcome from './components/welcome'
+import TodoList from './components/TodoList'
+import BookList from './components/BookList'
+import MovieList from './components/MovieList'
+
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Hello />
+      <Welcome />
+    
+    
+    </>
+  )
+}
+
+export default App
