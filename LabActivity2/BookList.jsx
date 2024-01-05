87f0868b57bdf16127c350c33f75708ffd617ae6
ivandayrit <100040157+ivan06292002@users.noreply.github.com>
2024-01-05 15:23:48 +0800

    Add files via upload

---
+++ LabActivity2/BookList.jsx
@@ -0,0 +1,14 @@
+export default function BookList(){
+    return(
+        <div>
+           <h1>BOOK LIST</h1>
+          <ul>
+              <li>Harry Potter</li>
+              <li>Fourth Wing</li>
+              <li>Better Days</li>
+    
+              
+          </ul>
+        </div>
+    );
+}
